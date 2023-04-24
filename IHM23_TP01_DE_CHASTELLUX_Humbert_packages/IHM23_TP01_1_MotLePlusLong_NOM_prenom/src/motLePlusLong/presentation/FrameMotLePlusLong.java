0ea8c341cf24b009d72b480e80f8319285b01304
BebertDeChast <hdechastellux@gmail.com>
2023-04-24 14:45:50 +0200

    end of 1.1

--- IHM23_TP01_DE_CHASTELLUX_Humbert_packages/IHM23_TP01_1_MotLePlusLong_NOM_prenom/src/motLePlusLong/presentation/FrameMotLePlusLong.java
+++ IHM23_TP01_DE_CHASTELLUX_Humbert_packages/IHM23_TP01_1_MotLePlusLong_NOM_prenom/src/motLePlusLong/presentation/FrameMotLePlusLong.java
@@ -12,6 +12,28 @@ public class FrameMotLePlusLong extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
 
+        // panneau nord
+        JPanel panneauNord = new JPanel();
+        panneauNord.setLayout(new BorderLayout());
+        JLabel tirage = new JLabel("Tirage :");
+        tirage.setToolTipText("Veuillez indiquer les lettres du tirage");
+        panneauNord.add(tirage, BorderLayout.WEST);
+
+        // text field
+        panneauNord.add(new JTextField("ETS"), BorderLayout.CENTER);
+
+        this.add(panneauNord, BorderLayout.NORTH);
+
+        // list
+        String[] test = { "ES", "ET", "SE", "TE", "EST", "SET", "TES" };
+        JList<String> testj = new JList<String>(test);
+        testj.setLayoutOrientation(JList.VERTICAL);
+        testj.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane scroll = new JScrollPane(testj);
+        this.add(scroll, BorderLayout.CENTER);
+
+        this.pack();
+
     }
 
 }
